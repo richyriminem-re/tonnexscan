9fe79edee3efa651c12a4523de20c39fd472eb55
richyriminem-re <richyesther378@gmail.com>
2025-08-16 08:32:55 +0000

    Add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react' // ✅ add this
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />  {/* ✅ mount analytics at the root */}
   </React.StrictMode>
 );
+
